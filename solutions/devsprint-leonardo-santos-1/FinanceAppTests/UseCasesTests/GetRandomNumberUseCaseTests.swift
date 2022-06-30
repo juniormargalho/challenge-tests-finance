c7dcab0b85837c47c7f49ca453522412992a8791
Junior Margalho <nelson.junior@usemobile.xyz>
2022-06-30 18:35:53 -0300

    🧩: teste range between

--- solutions/devsprint-leonardo-santos-1/FinanceAppTests/UseCasesTests/GetRandomNumberUseCaseTests.swift
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/UseCasesTests/GetRandomNumberUseCaseTests.swift
@@ -5,8 +5,27 @@
 //  Created by Junior Margalho on 30/06/22.
 //
 
-import Foundation
+import XCTest
 
-final class GetRandomNumberUseCaseTests {
+@testable import FinanceApp
+
+final class GetRandomNumberUseCaseTests: XCTestCase {
+    private lazy var sut = GetRandomNumberUseCase()
+    private lazy var retuned = sut.execute(from: Constants.initial, to: Constants.final)
     
+    func testReturnEqualsInteger() {
+        XCTAssertTrue(retuned is Int)
+    }
+
+    func testIntegerReturnedBetweenRangeInputs() {
+        let range = Constants.initial...Constants.final
+        XCTAssertTrue(range.contains(retuned))
+    }
+}
+
+private extension GetRandomNumberUseCaseTests {
+    enum Constants {
+        static let initial: Int = 0
+        static let final: Int = 9
+    }
 }
