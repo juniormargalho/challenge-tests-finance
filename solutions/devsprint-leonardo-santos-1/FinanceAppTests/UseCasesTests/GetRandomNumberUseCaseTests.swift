525dacce2de699b45e1c35192d4283a95e65ac44
Junior Margalho <nelson.junior@usemobile.xyz>
2022-06-30 16:52:13 -0300

    🧩: create arquive

---
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/UseCasesTests/GetRandomNumberUseCaseTests.swift
@@ -0,0 +1,12 @@
+//
+//  GetRandomNumberUseCaseTests.swift
+//  FinanceAppTests
+//
+//  Created by Junior Margalho on 30/06/22.
+//
+
+import Foundation
+
+final class GetRandomNumberUseCaseTests {
+    
+}
